edfe8ee5639885cea7b1702aa2a8d8843cd3bb0d
Malhar0505 <86611248+Malhar0505@users.noreply.github.com>
2022-07-20 17:50:31 +0530

    Project 123 , 124 files uploaded

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    video = createCapture(VIDEO);
+    video.size(550, 500);
+    canvas = createCanvas(550 ,500);
+    canvas.position(560 ,150);
+    poseNet = ml5.poseNet(video , modelLoaded);
+    poseNet.on('pose', gotPoses); 
+}
+function modelLoaded(){
+    console.log('PoseNet model is being Initialized')
+}
+function draw(){
+    background('#F7D8BA');
+}
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+    }    
+}
